25363381e4d21ef736227ba3d6e73230aa5793ce
Derek Nutile <dereknutile@gmail.com>
2016-01-20 13:11:46 -1000

    Changes successful logins to route to home.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/im-in';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new authentication controller instance.
