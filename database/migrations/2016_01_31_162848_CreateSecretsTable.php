9789405f61d35c2abfb2880e16a5416783949ec7
Derek Nutile <dereknutile@gmail.com>
2016-01-31 16:44:45 -0800

    Initializes secrets migration, controller, and index view.

---
+++ database/migrations/2016_01_31_162848_CreateSecretsTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSecretsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secrets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('user');
+            $table->string('password');
+            $table->string('url');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('secrets');
+    }
+}
---
+++ resources/views/secrets/index.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts/medium')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <h5>Secrets View</h5>
+    </div>
+</div>
+
+@stop
