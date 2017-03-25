5c8b67ee63e15d15fc881fa4275d580b2a0847de
Derek Nutile <dereknutile@gmail.com>
2017-03-25 06:24:47 -0700

    n/php Drop in and edit scripts include

---
+++ project/resources/views/_includes/scripts.blade.php
@@ -0,0 +1,15 @@
+
+<!-- javascripts -->
+<script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/select2/dist/js/select2.full.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/parsleyjs/dist/parsley.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/media/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/media/js/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/moment/min/moment.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/highcharts/highcharts.js') }}"></script>
+<script src="{{ asset('assets/vendor/highcharts/modules/exporting.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+<script src="{{ asset('assets/js/report.js') }}"></script>
