a77bc2d4eb2f16551864e40e7b941a673c2fc09c
Derek Nutile <dereknutile@gmail.com>
2017-03-25 07:54:45 -0700

    e/php Build simple footer

---
+++ project/resources/views/_includes/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <div>
+            <p class="text-center">Copyright &copy; {{ Carbon\Carbon::now()->format('Y') }} &mdash; {{ env('APP_TITLE') }}</p>
+            <p class="text-center">Maintained by <a href="{{ env('APP_AUTHOR_URL') }}" target="_blank">{{ env('APP_AUTHOR') }}</a> on <a href="{{ env('APP_REPO') }}" target="_blank">Github</a>.</p>
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.container -->
+</footer>
