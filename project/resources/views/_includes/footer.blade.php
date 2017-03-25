6b896b4389ceb2f26dcb4ff5900ffd3f4dcb2f54
Derek Nutile <dereknutile@gmail.com>
2017-03-25 12:32:58 -0700

    e/php Update footer

--- project/resources/views/_includes/footer.blade.php
+++ project/resources/views/_includes/footer.blade.php
@@ -1,8 +1,12 @@
 <footer class="footer">
     <div class="container">
         <div>
-            <p class="text-center">Copyright &copy; {{ Carbon\Carbon::now()->format('Y') }} &mdash; {{ env('APP_TITLE') }}</p>
-            <p class="text-center">Maintained by <a href="{{ env('APP_AUTHOR_URL') }}" target="_blank">{{ env('APP_AUTHOR') }}</a> on <a href="{{ env('APP_REPO') }}" target="_blank">Github</a>.</p>
+            <p class="text-center">Copyright &copy; {{ Carbon\Carbon::now()->format('Y') }} &mdash; {{ Config::get('app.app_title') }}</p>
+            <p class="text-center">Maintained by <a href="{{ Config::get('app.app_author_url') }}" target="_blank">{{ env('APP_AUTHOR') }}</a> on <a href="{{ Config::get('app.app_repo') }}" target="_blank">Github</a>.</p>
+            <p class="text-center">
+                <a href="/about">About</a>&nbsp;
+                <a href="/contact">Contact</a>
+            </p>
         </div>
         <!-- /.content -->
     </div>
