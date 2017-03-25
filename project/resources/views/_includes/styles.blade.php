d6774388f76e5d1718a38384f9b28c56c9a65b81
Derek Nutile <dereknutile@gmail.com>
2017-03-25 06:25:01 -0700

    n/php Drop in and edit styles include

---
+++ project/resources/views/_includes/styles.blade.php
@@ -0,0 +1,8 @@
+<!-- styles -->
+<link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/dist/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/fontawesome/css/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/select2/dist/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/datatables/media/css/dataTables.bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/animate.css/animate.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
