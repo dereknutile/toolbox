121829e31b2ccecf25f9da9b089f8b244e5f83bb
Derek Nutile <dereknutile@gmail.com>
2017-04-17 16:01:11 -0700

    r/php Remove unused view pages

--- project/resources/views/pages/docs.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('_layouts/app')
-
-@section('content')
-    <div class="container">
-        <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt numquam doloremque quos ab totam porro minus enim dignissimos incidunt quaerat eaque iste animi id, in labore illum sapiente minima. Eum.</p>
-        </div>
-    </div>
-@stop
